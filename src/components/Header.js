706a2e5287d701464ad41c8476576826a2e811e0
ck6888 <chandan6888.kumar@gmail.com>
2025-01-16 21:16:42 -0500

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,10 @@ function Header(props) {
     <div>
       <div className="add-to-cart">
         <span className="cart-count">{props.data.length}</span>
-        <img src="https://as2.ftcdn.net/jpg/03/11/48/85/1000_F_311488581_Yu4WPdLLNt47cymWU3VmHmOnH7BHqp9F.jpg" />
+        <img
+          alt="test"
+          src="https://as2.ftcdn.net/jpg/03/11/48/85/1000_F_311488581_Yu4WPdLLNt47cymWU3VmHmOnH7BHqp9F.jpg"
+        />
       </div>
     </div>
   );
